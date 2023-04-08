2cf03ec81b659cd3e1a896afa915f3d830264c34
Adel Amgad Salama <60940565+polaamgad88@users.noreply.github.com>
2023-04-08 12:40:42 +0200

    adding account privileges for Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,42 +3,51 @@ import { useNavigate } from 'react-router-dom'
 import Navbar from "./Navbar";
 
 const Main = () => {
-  const navigate = useNavigate();
-  const [checked, setChecked] = useState(false);
-  useEffect(() => {
-    var status = false;
-    console.log('Bearer ' + localStorage.getItem('token'))
-    fetch('http://192.168.1.31:4000/main', {
-      method: 'get',
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": 'Bearer ' + localStorage.getItem('token')
-      },
-    })
-      .then(
-        (response) => {
-          return response.json()
-        }
-      )
-      .then(
-        (responseJson) => {
-          status = responseJson.success
-          if (status) {
-            setChecked(true)
-            console.log("access gained")
-          }
-          else {
-            console.log("no access to open this page")
-            navigate("/Login")
-          }
-        })
-      .catch(
-        (error) => {
-          console.log(error);
-        });
-  })
-  if (!checked) return null;
+  {/*variables for account level should get this values at login stage */}
+  let researcher = false;
+  let doctor = true;
+  let representative = true;
+
+  let Admin = true;
+
+ //BACK FARKHA 
+ //------------------------
+ const navigate = useNavigate();
+ const [checked, setChecked] = useState(false);
+ useEffect(() => {
+   var status = false;
+   console.log('Bearer ' + localStorage.getItem('token'))
+   fetch('http://192.168.1.31:4000/main', {
+     method: 'get',
+     headers: {
+       "Content-Type": "application/json",
+       "Accept": "application/json",
+       "Authorization": 'Bearer ' + localStorage.getItem('token')
+     },
+   })
+     .then(
+       (response) => {
+         return response.json()
+       }
+     )
+     .then(
+       (responseJson) => {
+         status = responseJson.success
+         if (status) {
+           setChecked(true)
+           console.log("access gained")
+         }
+         else {
+           console.log("no access to open this page")
+           navigate("/Login")
+         }
+       })
+     .catch(
+       (error) => {
+         console.log(error);
+       });
+ })
+ if (!checked) return null;
   return (
     <div class="h-screen bg-blue-500">
     <Navbar/>
@@ -48,68 +57,52 @@ const Main = () => {
         md:grid-cols-3  lg:grid-cols-3 xl:grid-cols-3 gap-16 justify-center">
 
 
-
-          <div class=" rounded overflow-hidden shadow-none">
+         {/* doctor is only have access to add record */}
+          <div class=" rounded overflow-hidden shadow-none"
+          className={`${doctor ? 'block' : 'hidden'}`}>
             <a href="/Add_rec">
               <button>
-                <img
-                  class="w-64 h-64flex items-center justify-center rounded-full
-       transition-transform duration-500 transform hover:scale-75"
-                  src={require("./imgs/AddRec.jpg")}
-                  alt="logo" />
+                <img class="w-64 h-64flex items-center justify-center rounded-full transition-transform duration-500 transform hover:scale-75"
+                  src={require("./imgs/AddRec.jpg")}  alt="logo" />
                 <p class=" hover:text-neutral-900 hover:font-extrabold font-sans text-lg font-bold
                 text-white mt-3 mb-3 py-2 px-10   rounded-lg focus:outline-none focus:shadow-outline">
                   Add Record</p></button></a>
           </div>
 
-          <div class=" rounded overflow-hidden shadow-none">
 
 
+           {/* researcher OR representative of ministry have access to view alerts */}
+          <div class=" rounded overflow-hidden shadow-none"
+          className={`${researcher || representative ? 'block' : 'hidden'}`}>
             <a href="/Dashboard">
               <button>
-                <img
-                  class="w-64 h-64flex items-center justify-center rounded-full
-       transition-transform duration-500 transform hover:scale-75
-       "
-                  src={require("./imgs/Dashb.jpg")}
-                  alt="logo"
-                />
-                <div class="py-2 px-10 mr-20 ">
-                  <p
-                    class=" hover:text-neutral-900 hover:font-extrabold font-sans text-lg font-bold  text-white mt-3 mb-3 
-      
-        py-2 px-10   rounded-lg focus:outline-none focus:shadow-outline"
-                  >
+                <img class="w-64 h-64flex items-center justify-center 
+                rounded-full transition-transform duration-500 transform hover:scale-75" src={require("./imgs/Dashb.jpg")}
+                alt="logo" />
+                
+                  <p class="mt-5  hover:text-neutral-900 hover:font-extrabold font-sans text-lg font-bold  text-center 
+                text-white  inline-block  rounded-lg focus:outline-none focus:shadow-outline">
                     View Dashboard
-                  </p>
-                </div>
-              </button>
-            </a>
+                  </p></button></a>
           </div>
 
-          <div class=" rounded overflow-hidden shadow-none">
 
-
-            <a href="/Alerts">
+      {/* doctor and representative of ministry are only have access to view alerts */}
+          <div class=" rounded overflow-hidden shadow-none" 
+          className={`${ representative ? 'block' : 'hidden'}`}>
+            <a href="/Alerts" >
               <button>
                 <img
-                  class="w-64 h-64flex items-center justify-center rounded-full
-       transition-transform duration-500 transform hover:scale-75
-       "
-                  src={require("./imgs/warnin.jpg")}
-                  alt="logo"
-                />
-
-                <p
-                  class="  mt-3 mb-3 
-                hover:text-neutral-900 hover:font-extrabold font-sans text-lg font-bold  text-white
-       py-2 px-10  w-52 rounded-lg focus:outline-none focus:shadow-outline"
-                >
-                  View Alerts
-                </p>
-              </button>
-            </a>
+                  class="w-64 h-64flex items-center justify-center rounded-full transition-transform duration-500 transform hover:scale-75"
+                  src={require("./imgs/warnin.jpg")} alt="logo"/>
+                <p class="mt-3 mb-3 text-center inline-block  hover:text-neutral-900 hover:font-extrabold font-sans text-lg font-bold 
+                   text-white py-2 px-10  w-52 rounded-lg focus:outline-none focus:shadow-outline">
+                  View Alerts </p></button></a>
           </div>
+
+
+
+
         </div>
       </body>
     </div>
