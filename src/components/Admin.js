005d19d03a853f739a376c5b26781b751ce8e5b0
kareems3fan <101336843+kareems3fan@users.noreply.github.com>
2023-04-14 14:53:59 +0200

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -49,51 +49,64 @@ const Admin = () => {
         });
   })
   if (!checked) return null;
-  return (
+ return (
     <div class="h-screen bg-blue-500">
-      <Navbar />
-      <body class=" bg-blue-500 ">
-        <div class="p-10 md:flex md:justify-center mx-auto grid grid-cols-1 sm:grid-cols-1
-     md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-14">
 
-          <div class=" rounded overflow-hidden shadow-none mt-20">
-            <button>
-              <a href="/Add_doc">
-                <img class="w-64 h-64flex items-center justify-center rounded-full
+    <body class=" bg-blue-500 ">
+    
+    <div class="p-10 ml-44 mr-16  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-28">
+
+
+
+      <div class=" rounded overflow-hidden shadow-none mt-20">
+      <button>
+        <a href="/adddoctor">
+       <img  class="w-64 h-64flex items-center justify-center rounded-full
        transition-transform duration-500 transform hover:scale-75
-       " src={
-                    require('./imgs/1114190.png')}
-                  alt="logo" />
-              </a>
+       " src={require('./1114161.png')} 
+        alt="logo"/>
+        </a>
 
-              <a href="/Add_doc">
-                <p class=" hover:text-white text-neutral-900 mt-3 mb-3 
+          <a href="/adddoctor"> 
+       <p class=" hover:text-white text-neutral-900 mt-3 mb-3 
         font-mono text-lg font-extrabold 
           py-2 px-10   rounded-lg focus:outline-none focus:shadow-outline">Add Doctor profile</p>
-              </a>
-            </button>
+          </a>
+      </button>
+      
+      </div>
+    
 
-          </div>
 
 
-          <div class=" rounded overflow-hidden shadow-none my-20">
-            <a href=" ">
-              <button>
-                <img class="w-64 h-64flex items-center justify-center rounded-full
+     <div class=" rounded overflow-hidden shadow-none my-20">
+      <button>
+      <a href="/search">
+       <img  class="w-64 h-64flex items-center justify-center rounded-full
        transition-transform duration-500 transform hover:scale-75
-       " src={require('./imgs/1114161.png')}
-                  alt="logo" />
-                <div class=" mx-auto">
-                  <p class=" hover:text-white text-neutral-900 
+       " src={require('./1114190.png')} 
+        alt="logo"/>
+         </a>
+         <a href="/search">
+        <div class=" mx-auto">
+            <p class=" hover:text-white text-neutral-900 
        font-mono text-lg font-extrabold 
         w-72 rounded-lg focus:outline-none focus:shadow-outline">Edit doctor profile</p></div>
+       </a>
+      </button>
+      </div>
 
-              </button>
-            </a>
-          </div>
-        </div>
-      </body>
+
+
+
+   
+    
+      
     </div>
+  
+
+    </body>
+  </div>
   )
 }
 
