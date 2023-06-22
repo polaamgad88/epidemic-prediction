03a07fd445e60ffce06d75adbbc64984d248f079
marawanFawzy <marawan20191700616@cis.asu.edu.eg>
2023-06-22 12:26:28 +0300

    refactor

--- src/components/Add_rec.js
+++ src/components/Add_rec.js
@@ -251,7 +251,7 @@ and value, onchange to be edited..
               </label>
               <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
              border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white
-              focus:border-gray-500" type="number" placeholder="ex. 3"
+              focus:border-gray-500" type="number" placeholder="ex. 3" min="0"
                 id='sym_days'
               />
             </div>
--- src/components/Edit_doc.js
+++ src/components/Edit_doc.js
@@ -287,7 +287,7 @@ const Edit_doc = () => {
                 </label>
                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 
       rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="Did" type="text" placeholder="ex. 28/8/1995" onChange={(e) => Setbirth(e.target.value)}
+                  id="Did" type="date" placeholder="ex. 28/8/1995" onChange={(e) => Setbirth(e.target.value)}
                   value={birth} />
 
               </div>
