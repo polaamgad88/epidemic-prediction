2cff4b86d1ebae032be1ce15d237e4956076db1e
kareems3fan <101336843+kareems3fan@users.noreply.github.com>
2023-06-24 23:47:29 +0300

    navbar-admin modifications

--- src/components/Navbar_Admin.js
+++ src/components/Navbar_Admin.js
@@ -38,15 +38,15 @@ function Navbar() {
   };
 
   return (
-    <nav class="flex justify-between px-10 py-2 items-center bg-blue-500" >
+    <nav class="flex justify-between px-10 py-2 items-center bg-white rounded-sm" >
       <a href="/">
-        <img class="w-14 h-14" src={require('./imgs/logo.png')} alt="logo" />
+        <img class="w-8 h-8" src={require('./imgs/logo.png')} alt="logo" />
       </a>
       <div class="flex items-center">
         <ul class="flex items-center space-x-12">
           {isLoggedIn ? (
             <>
-              <li class="font-mono text-sm font-semibold text-slate-800 hover:text-white  ">
+              <li class="font-mono text-sm font-semibold text-slate-800 hover:animate-pulse  ">
                 <a href="/Admin">
                   <button
                     class="hover:animate-pulse"
@@ -55,7 +55,7 @@ function Navbar() {
                   </button>
                 </a>
               </li>
-              <li class="font-mono text-sm font-semibold text-slate-800 hover:text-white ">
+              <li class="font-mono text-sm font-semibold text-slate-800 hover:animate-pulse ">
                 <button
                   class="hover:animate-pulse"
                   type="button"
