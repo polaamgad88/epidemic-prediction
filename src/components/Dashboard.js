d943ac12a77715f2fadbb21e461123bc313b672e
kareems3fan <101336843+kareems3fan@users.noreply.github.com>
2023-06-26 17:39:23 +0300

    dashboard modifications

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -299,14 +299,14 @@ const Dashboard = () => {
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-year">
                   First year
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" placeholder="2019" onChange={(e) => SetYear1(e.target.value)}
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" placeholder="2019" 
                   value={year1} />
               </div>
               <div class="w-full md:w-1/2 px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-second-year">
                   Second year
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="2020" onChange={(e) => SetYear2(e.target.value)}
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="2020" 
 
                   value={year2} />
               </div>
