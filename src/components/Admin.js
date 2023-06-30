4f3d1e677c34e2f44b8f1d0a5194ed5315cc4a43
Adel Amgad <polaamgad288@gmail.com>
2023-06-30 15:04:34 +0300

    Website design edition

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -48,8 +48,8 @@ const Admin = () => {
         });
   },[navigate])
   if (!checked) return (
-    <div className="h-screen flex justify-center items-center bg-blue-600">
-      <div className="p-10 bg-blue-800 rounded-lg shadow-xl">
+    <div className="h-screen flex justify-center items-center bg-blue-200 ">
+      <div className="p-10 bg-blue-200 rounded-lg shadow-xl">
         <svg className="animate-spin h-12 w-12 text-gray-500 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm10 0l3 2.647A7.962 7.962 0 0120 "></path>
@@ -59,63 +59,38 @@ const Admin = () => {
     </div>
   );
   return (
-    <div class="h-screen bg-gradient-to-r from-blue-300 to-white">
+    <div class="h-screen bg-blue-200">
       <Navbar/>
 
-      <body class=" bg-gradient-to-r from-blue-300 to-white ">
+      <body class="bg-blue-200 flex items-center justify-center">
+  <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-40">
 
-        <div class="p-10 ml-44 mr-16  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-28">
+    <div class="rounded overflow-hidden shadow-none mt-20">
+      <button>
+        <a href="/Add_doc">
+          <img class="w-64 h-64 flex items-center justify-center rounded-full transition-transform duration-500 transform hover:scale-75" src={require('./imgs/1114161.png')} alt="logo" />
+        </a>
+        <a href="/Add_doc">
+          <p class="hover:text-white text-neutral-900 mt-3 mb-3 font-mono text-lg font-extrabold py-2 px-10 rounded-lg focus:outline-none focus:shadow-outline">Add Doctor profile</p>
+        </a>
+      </button>
+    </div>
 
-
-
-          <div class=" rounded overflow-hidden shadow-none mt-20">
-            <button>
-              <a href="/Add_doc">
-                <img class="w-S64 h-64flex items-center justify-center rounded-full
-       transition-transform duration-500 transform hover:scale-75
-       " src={require('./imgs/1114161.png')}
-                  alt="logo" />
-              </a>
-
-              <a href="/Add_doc">
-                <p class=" hover:text-white text-neutral-900 mt-3 mb-3 
-        font-mono text-lg font-extrabold 
-          py-2 px-10   rounded-lg focus:outline-none focus:shadow-outline">Add Doctor profile</p>
-              </a>
-            </button>
-
+    <div class="rounded overflow-hidden shadow-none my-20">
+      <button>
+        <a href="/search">
+          <img class="w-64 h-64 flex items-center justify-center rounded-full transition-transform duration-500 transform hover:scale-75" src={require('./imgs/1114190.png')} alt="logo" />
+        </a>
+        <a href="/search">
+          <div class="mx-auto mt-5">
+            <p class="hover:text-white text-neutral-900 font-mono text-lg font-extrabold w-72 rounded-lg focus:outline-none focus:shadow-outline">Edit doctor profile</p>
           </div>
+        </a>
+      </button>
+    </div>
 
-
-
-
-          <div class=" rounded overflow-hidden shadow-none my-20">
-            <button>
-              <a href="/search">
-                <img class="w-64 h-64flex items-center justify-center rounded-full
-       transition-transform duration-500 transform hover:scale-75
-       " src={require('./imgs/1114190.png')}
-                  alt="logo" />
-              </a>
-              <a href="/search">
-                <div class=" mx-auto">
-                  <p class=" hover:text-white text-neutral-900 
-       font-mono text-lg font-extrabold 
-        w-72 rounded-lg focus:outline-none focus:shadow-outline">Edit doctor profile</p></div>
-              </a>
-            </button>
-          </div>
-
-
-
-
-
-
-
-        </div>
-
-
-      </body>
+  </div>
+</body>
     </div>
   )
 }
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div class=" ">
 
-      <main class="h-screen w-full flex flex-col justify-center items-center bg-gradient-to-r from-blue-300 to-white">
+      <main class="h-screen w-full flex flex-col justify-center items-center bg-blue-200">
         <h1 class="text-12xl font-extrabold from-blue-400 via-blue-500 to-blue-600 tracking-widest">404</h1>
         <div class="bg-[#FF6A3D] px-2 text-xl rounded rotate-12 absolute">
           Page Not Found
